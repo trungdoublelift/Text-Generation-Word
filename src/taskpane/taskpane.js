a7b9d44babbb63e162c9b9fb4fa6c6791bdba925
trungdoublelift <trungdoublelift@gmail.com>
2022-11-24 14:17:12 +0700

    .

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -13,6 +13,7 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+
   }
 });
 
@@ -20,35 +21,57 @@ export async function run() {
 
   return Word.run(async (context) => {
     //Khai báo vv.
-    let numOfResults = document.getElementById("numOfResults").value;
+
     let resultContainer = document.getElementById("result-container");
-    let buttonRun=document.getElementById("run");
+    let buttonRun = document.getElementById("run");
     let wait = document.createElement("p");
+    let status = document.getElementById("status-text");
+    let text = document.getElementById("keyword").value;
+    let numOfResult = document.getElementById("numOfResult").value;
+    let textLength = document.getElementById("numOfText").value;
     //Thay đổi trạng thái
-    wait.innerText = `Đang chờ ${numOfResults} kết quả .....`;
-    buttonRun.disabled=true;
+    if (!checkValid(numOfResult)) {
+      status.innerText = "Số kết quả phải lớn hơn 0";
+      return;
+    }
+    if (!checkValid(textLength)) {
+      status.innerText = "Độ dài văn bản phải lớn hơn 0";
+      return;
+    }
+    status.innerText = "Đang xử lý...";
+    wait.innerText = `Đang chờ ${numOfResult} kết quả .....`;
+
+    buttonRun.disabled = true;
     resultContainer.appendChild(wait);
     // call API generation
-    let result = await userAction();
+    let result = await userAction(text, numOfResult, textLength);
+    if (result === "error") {
+      status.innerText = "Lỗi kết nối đến server";
+      buttonRun.disabled = false;
+      resultContainer.removeChild(wait);
+      return;
+    }
     resultContainer.removeChild(wait);
+    status.innerText = "Đã xong";
     // in ra kết quả
-    for (let i = 0; i < numOfResults; i++) {
+    for (let i = 0; i < result.text.length; i++) {
       let resultTag = document.createElement("p");
-      resultTag.innerText =`Kết quả ${i+1}:${result.text}`;
+      resultTag.innerText = `Kết quả ${i + 1}:\n${result.text[i]}`;
       resultContainer.appendChild(resultTag);
     }
-    buttonRun.disabled=false;
+    buttonRun.disabled = false;
     await context.sync();
   });
 }
-async function userAction() {
+async function userAction(text, numOfResult, textLength) {
   const rawResponse = await fetch('http://localhost:8080/generate', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ text: document.getElementById("keyword").value }),
+    body: JSON.stringify({ text: text, numOfResult: parseInt(numOfResult), textLength: parseInt(textLength) })
+
   });
   if (rawResponse.ok) {
     return rawResponse.json();
@@ -57,3 +80,10 @@ async function userAction() {
     return "error"
   }
 }
+function checkValid(input) {
+  if (parseInt(input) < 0) {
+    return false;
+  }
+  return true;
+}
+
